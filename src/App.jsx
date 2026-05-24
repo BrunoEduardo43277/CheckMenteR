36dc4501af1d49ff18716fbf843bee2c621c5cae
BrunoEduardo43277 <be43277@gmail.com>
2026-05-24 11:55:33 -0300

    atualização das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,37 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Cadastro from "./pages/Cadastro.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Checkin from "./pages/Checkin.jsx";
-import Perfil from "./pages/Perfil.jsx";
-import IA from "./pages/IA.jsx";
-import Relatorios from "./pages/Relatorios.jsx";
-import Alertas from "./pages/Alertas.jsx";
-import AppLayout from "./layouts/AppLayout.jsx";
-import Configuracoes from "./pages/Configuracoes.jsx";
-import Alunos from "./pages/Alunos.jsx";
-import Mensagens from "./pages/mensagen.jsx";
-import Respirador from "./pages/acaoDashboard/respirador.jsx";
-import RespiracaoT1 from "./pages/acaoDashboard/tiposRespiracao/respiracaoT1.jsx";
+import Login from "./pages/auth/Login.jsx";
+import Cadastro from "./pages/auth/Cadastro.jsx";
+import Dashboard from "./pages/dashboard/Dashboard.jsx";
+import Checkin from "./pages/checkin/Checkin.jsx";
+import Diario from "./pages/dashboard/tiposfuncao/diario.jsx";
+import Alunos from "./pages/alunos/Alunos.jsx";
+import Perfil from "./pages/perfil/Perfil.jsx";
+import Configuracoes from "./pages/configuracao/Configuracoes.jsx";
+import IA from "./pages/ia/IA.jsx";
+import Relatorios from "./pages/relatorios/Relatorios.jsx";
+import Mensagens from "./pages/mensage/mensagen.jsx";
+import Alertas from "./pages/alerta/Alertas.jsx";
+import Respirador from "./pages/dashboard/tiposfuncao/respirador.jsx";
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-100">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/checkin" element={<Checkin />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/ia" element={<IA />} />
-        <Route path="/relatorios" element={<Relatorios />} />
-        <Route path="/alertas" element={<Alertas />} />
-        <Route path="/configuracoes" element={<Configuracoes />} />
-        <Route path="/alunos" element={<Alunos />} />
-        <Route path="/mensagens" element={<Mensagens />} />
-        <Route path="/respirador" element={<Respirador />} />
-        <Route path="/respiracaoT1" element={<RespiracaoT1 />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/checkin" element={<Checkin />} />
+      <Route path="/diario" element={<Diario />} />
+      <Route path="/alunos" element={<Alunos />} />
+      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/configuracoes" element={<Configuracoes />} />
+      <Route path="/ia" element={<IA />} />
+      <Route path="/relatorios" element={<Relatorios />} />
+      <Route path="/mensagens" element={<Mensagens />} />
+      <Route path="/alertas" element={<Alertas />} />
+      <Route path="/respirador" element={<Respirador />} />
+    </Routes>
   );
-}
-
-
-
-export default App;
+} 
